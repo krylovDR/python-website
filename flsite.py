1e34bf5b5ec9ea50f0c6aa000ad0f775683378e2
krylovDR <daniil12244892@mail.ru>
2022-06-06 12:44:02 +0300

    DataBase 1 commit

--- flsite.py
+++ flsite.py
@@ -1,8 +1,49 @@
-from flask import Flask, render_template, request, abort, flash, session, redirect, url_for
+from flask import Flask, render_template, g, request, abort, flash, session, redirect, url_for
+import sqlite3
+import os
 
+
+# конфигурация
+DATABASE = '/tmp/flsite.db'  # путь до файла базы данных
+DEBUG = True  # режим отладки
+SECRET_KEY = '8y23h(*#@g8f9232hfjsdf23f(#*'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '9823hiuufsjdbn8iuhlafkef872g3f'
+app.config.from_object(__name__)  # загрузка конфигураций
+
+# переопределение пути к базе данных в рабочий каталог
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+
+# общая функция для установления соединения с базой данных
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row  # представить в виде словаря, а не картежа
+    return conn
+
+
+# вспомогательная функция для создания таблиц БД
+def create_db():
+    db = connect_db()  # подключение к бд
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())  # запуск скриптов, содержащихся в sq_db.sql
+    db.commit()  # запись изменений в базу данных
+    db.close()
+
+
+# соединение с БД, если оно ещё не установлено
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+# закрытие соединения с базой данных
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
 
 menu = [{"name": "Загрузка кода", "url": "upload-code"},
         {"name": "Личный кабинет", "url": "personal-area"},
@@ -12,6 +53,7 @@ menu = [{"name": "Загрузка кода", "url": "upload-code"},
 # декоратор с url-адресом обработчика (/ - главная страница)
 @app.route("/")
 def index():
+    db = get_db()
     return render_template('index.html', menu=menu)
 
 
